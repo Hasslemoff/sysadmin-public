b337583d01b6efdcd781cbbec6ddcf6da6813a03
joe.moffitt <joe.moffitt@moffcorp.co.uk>
2023-12-28 18:17:30 +0000

    Add diff UserID

--- Intune/MacOS/Scripts/MacOS-DeviceConfig-CreateITLocalAdminV3.zsh
+++ Intune/MacOS/Scripts/MacOS-DeviceConfig-CreateITLocalAdminV3.zsh
@@ -1,12 +1,12 @@
 #!/bin/zsh
 # Username and Password to create
-username=ADMINACCOUNTUSERNAME
+username=ITAdminAccountUserName
 password="SuperSecurePassword3!"
 # Create User and add to admins
 dscl . -create /Users/$username
 dscl . -create /Users/$username UserShell /bin/bash # Sets Default Shell could be sh or zsh
 dscl . -create /Users/$username RealName $username # Sets Displayname for the account
-dscl . -create /Users/$username UniqueID "666" #Unique Local ID
+dscl . -create /Users/$username UniqueID "777" #Unique Local ID
 dscl . -create /Users/$username PrimaryGroupID 20 # 20 for admin accounts 80 for standard accounts
 dscl . -create /Users/$username NFSHomeDirectory /Users/$username # Creates account home directory
 dscl . -passwd /Users/$username $password # Sets PW
