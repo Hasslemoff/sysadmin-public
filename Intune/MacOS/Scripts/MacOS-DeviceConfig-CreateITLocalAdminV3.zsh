b591daf043bf8ad0c228e67951fd39a02584d119
joe.moffitt <joe.moffitt@moffcorp.co.uk>
2023-12-28 17:08:00 +0000

    Add V3

---
+++ Intune/MacOS/Scripts/MacOS-DeviceConfig-CreateITLocalAdminV3.zsh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+# Username and Password to create
+username=ADMINACCOUNTUSERNAME
+password="SuperSecurePassword"
+# Create User and add to admins
+dscl . -create /Users/$username
+dscl . -create /Users/$username UserShell /bin/bash # Sets Default Shell could be sh or zsh
+dscl . -create /Users/$username RealName $username # Sets Displayname for the account
+dscl . -create /Users/$username UniqueID "666" #Unique Local ID
+dscl . -create /Users/$username PrimaryGroupID 20 # 20 for admin accounts 80 for standard accounts
+dscl . -create /Users/$username NFSHomeDirectory /Users/$username # Creates account home directory
+dscl . -passwd /Users/$username $password # Sets PW
+dscl . -append /Groups/admin GroupMembership $username # Adds to local admin group
+dscl . -create /Users/$username IsHidden 1 #Hides the user from the login screen
