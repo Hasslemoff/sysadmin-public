04705dff573f221d062292472fe4c798f0ebd31b
Hasslemoff <joe.moffitt@moffcorp.co.uk>
2025-02-02 20:51:16 -0500

    Add TCP Connections script.

---
+++ Powershell/CurrentTCPConnections.ps1
@@ -0,0 +1,1 @@
+Get-NetTCPConnection -State Established |Select-Object -Property LocalAddress, LocalPort,@{name='RemoteHostName';expression={(Resolve-DnsName $_.RemoteAddress).NameHost}},RemoteAddress, RemotePort, State,@{name='ProcessName';expression={(Get-Process -Id $_.OwningProcess). Path}},OffloadState,CreationTime |ft
