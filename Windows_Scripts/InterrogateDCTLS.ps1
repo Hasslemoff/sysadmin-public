029da6c9ce869f76483dbbcf194f4deb1de9f3e6
Hasslemoff <joe.moffitt@moffcorp.co.uk>
2025-10-30 20:47:24 -0400

    Remove

--- Windows_Scripts/InterrogateDCTLS.ps1
+++
@@ -1,4 +0,0 @@
-$server = "DC.domain.com" $port = 636
-$tcpClient = New-Object System.Net.Sockets.TcpClient($server, $port) $sslStream = New-Object System.Net.Security.SslStream($tcpClient.GetStream(), $false, ({ $true }))
-$sslStream.AuthenticateAsClient($server)
-$sslStream
