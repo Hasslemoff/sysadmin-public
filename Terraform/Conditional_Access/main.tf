1224887aace17a309b12a33c0276fcf176c7d395
Hasslemoff <joe.moffitt@moffcorp.co.uk>
2025-11-01 01:42:08 -0400

    Update CA example

--- Terraform/Conditional_Access/main.tf
+++ Terraform/Conditional_Access/main.tf
@@ -27,3 +27,62 @@ resource "msgraph_resource" "ca_exo_require_mfa" {
     }
   }
 }
+
+resource "msgraph_resource" "block_legacy_auth" {
+  url = "identity/conditionalAccess/policies"
+  body = {
+    displayName = "Via MSGraph - Block Legacy Authentication"
+    state       = "enabledForReportingButNotEnforced"
+    conditions = {
+      users = {
+        includeUsers  = ["All"]
+        excludeUsers  = []
+        includeGroups = []
+        excludeGroups = []
+        includeRoles  = []
+        excludeRoles  = []
+      }
+      applications = {
+        includeApplications                         = ["All"]
+        excludeApplications                         = []
+        includeUserActions                          = []
+        includeAuthenticationContextClassReferences = []
+        applicationFilter                           = null
+      }
+      clientAppTypes = [
+        "exchangeActiveSync",
+        "other"
+      ]
+      platforms = {
+        includePlatforms = ["all"]
+        excludePlatforms = []
+      }
+      locations = {
+        includeLocations = ["All"]
+        excludeLocations = []
+      }
+      authenticationFlows = {
+        transferMethods = "none"
+      }
+      signInRiskLevels           = []
+      userRiskLevels             = []
+      servicePrincipalRiskLevels = []
+    }
+    grantControls = {
+      operator                    = "OR"
+      builtInControls             = ["block"]
+      customAuthenticationFactors = []
+      termsOfUse                  = []
+      authenticationStrength      = null
+    }
+    sessionControls = {
+      applicationEnforcedRestrictions = null
+      cloudAppSecurity                = null
+      signInFrequency                 = null
+      persistentBrowser               = null
+      continuousAccessEvaluation      = null
+      secureSignInSession             = null
+      disableResilienceDefaults       = null
+    }
+  }
+}
